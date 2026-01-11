ebd7fcebfa769a1f898715acbe55a9e62a6b6b53
SnowvyTH <supanat617@gmail.com>
2026-01-11 20:39:04 +0700

    Day 1.1

--- 1-chapter/index.js
+++ 1-chapter/index.js
@@ -1,1 +1,18 @@
-console.log('Hello Javascript')
+// old way
+function testVar() {
+    if (true) {
+        var message = 'this is var message'
+    }
+    console.log(message)
+}
+
+testVar()
+
+for (var i = 0; i < 5; i++) {
+    console.log(i)
+}
+console.log(i)
+console.log('============================')
+for (let i = 0; i < 5; i++) {
+    console.log(i)
+}
