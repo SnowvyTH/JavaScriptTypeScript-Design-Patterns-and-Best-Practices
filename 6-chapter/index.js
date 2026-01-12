38e4c6300709becbbb951151474e5f5318dc1a2d
SnowvyTH <supanat617@gmail.com>
2026-01-12 22:32:11 +0700

    Day 1.3

---
+++ 6-chapter/index.js
@@ -0,0 +1,9 @@
+const [posts, users, randomUser] = await Promise.all([
+    fetch('https://jsonplaceholder.typicode.com/posts/1').then(response => response.json()),
+    fetch('https://jsonplaceholder.typicode.com/posts/2').then(response => response.json()),
+    fetch('https://randomuser.me/api/?results=3').then(response => response.json())
+])
+
+console.log(posts)
+console.log(users)
+console.log(randomUser)
