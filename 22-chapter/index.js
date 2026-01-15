971f0c36267a3fb5b55b046ec8014a080c13a79b
SnowvyTH <supanat617@gmail.com>
2026-01-15 23:09:24 +0700

    Day 2.5

--- 22-chapter/index.js
+++ 22-chapter/index.js
@@ -1,2 +1,13 @@
 const fullname = 'John';
 const age = 20;
+
+// old way
+const message = 'Hello ' + fullname + ' age: ' + age
+console.log(message)
+
+// new way
+const message2 = `Hello ${fullname} age: ${age}`
+console.log(message2)
+
+const message3 = `Hello ${fullname.toUpperCase()} age: ${age + 10}`
+console.log(message3)
